0112115524063d710068f182fd54aee217cb09dc
Joseph-san <78768864+Joseph-san@users.noreply.github.com>
2021-02-10 09:48:41 +0800

    Add files via upload

---
+++ Testing1/Testing1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testing1
+//
+//  Created by VANRIIX on 2/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
